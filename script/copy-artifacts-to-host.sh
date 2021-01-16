aa96432279e50b92b0325d746a3baad4079ce0e1
casey langen <casey.langen@gmail.com>
2021-01-16 11:32:24 -0800

    Script debug.

--- script/copy-artifacts-to-host.sh
+++ script/copy-artifacts-to-host.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [[ -z "${MUSIKCUBE_BUILD_HOST_IP}" ]]; then
+    echo "no build host ip specified"
     exit
 fi
 
 if [[ -z "${MUSIKCUBE_BUILD_HOST_PW}" ]]; then
+    echo "no build host pw specified"
     exit
 fi
+
+echo sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
 
 sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
