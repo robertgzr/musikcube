4ef710f1239d78db2a35bc269f514d489c68b1e3
casey langen <casey.langen@gmail.com>
2021-01-16 11:58:12 -0800

    More debugging.

--- script/copy-artifacts-to-host.sh
+++ script/copy-artifacts-to-host.sh
@@ -12,4 +12,7 @@ fi
 
 echo sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
 
+ping -c 5 192.168.1.218
+ping -c 5 ${MUSIKCUBE_BUILD_HOST_IP} 
+
 sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
