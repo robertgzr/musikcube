d8bcfb533bd7d0db2bae01e3cf5effcfcb2a17af
casey langen <casey.langen@gmail.com>
2021-01-16 12:06:33 -0800

    More debugging. I get the sense we can't easily escape localhost

--- script/copy-artifacts-to-host.sh
+++ script/copy-artifacts-to-host.sh
@@ -14,5 +14,6 @@ echo sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HO
 
 ping -c 5 192.168.1.218
 ping -c 5 ${MUSIKCUBE_BUILD_HOST_IP} 
+traceroute 192.168.1.218
 
 sshpass -p ${MUSIKCUBE_BUILD_HOST_PW} scp "$1/*" build@${MUSIKCUBE_BUILD_HOST_IP}:/home/build/ 2> /dev/null
